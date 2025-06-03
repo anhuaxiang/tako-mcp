bdac09b6bbc80e29f4517e129fa47a21512ef879
Dennis Ramdass <dennis@trytako.com>
2025-06-02 17:08:30 -0700

    Bump MCP version

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Any
 
 from tako.client import TakoClient, KnowledgeSearchSourceIndex
 from tako.types.visualize.types import TakoDataFormatDataset
-from mcp.server.fastmcp import FastMCP 
+from mcp.server.fastmcp import FastMCP
 
 TAKO_API_KEY = os.getenv("TAKO_API_KEY")
 X_TAKO_URL = os.getenv("X_TAKO_URL", "https://trytako.com")
@@ -16,6 +16,7 @@ ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
 mcp = FastMCP("tako", port=8001, host="0.0.0.0")
 tako_client = TakoClient(api_key=TAKO_API_KEY, server_url=X_TAKO_URL)
 
+
 @mcp.tool()
 async def search_tako(text: str) -> str:
     """Search Tako for any knowledge you want and get data and visualizations."""
@@ -23,14 +24,15 @@ async def search_tako(text: str) -> str:
         response = tako_client.knowledge_search(
             text=text,
             source_indexes=[
-                KnowledgeSearchSourceIndex.TAKO, 
-                KnowledgeSearchSourceIndex.WEB
-            ]
+                KnowledgeSearchSourceIndex.TAKO,
+                KnowledgeSearchSourceIndex.WEB,
+            ],
         )
     except Exception:
         logging.error(f"Failed to search Tako: {text}, {traceback.format_exc()}")
         return "No card found"
     return response.model_dump()
+
 
 @mcp.prompt()
 async def generate_search_tako_prompt(text: str) -> str:
@@ -58,6 +60,7 @@ async def generate_search_tako_prompt(text: str) -> str:
     </UserInputText>
     """
 
+
 @mcp.tool()
 async def visualize_dataset(dataset: dict[str, Any]) -> str:
     """Given a structured dataset in Tako Data Format, return a visualization."""
@@ -67,14 +70,16 @@ async def visualize_dataset(dataset: dict[str, Any]) -> str:
         logging.error(f"Invalid dataset format: {dataset}, {traceback.format_exc()}")
         # If a dataset is invalid, return the traceback to the client so it can retry with the correct format
         return f"Invalid dataset format: {dataset}, {traceback.format_exc()}"
-    
+
     try:
         response = tako_client.beta_visualize(tako_dataset)
     except Exception:
-        logging.error(f"Failed to generate visualization: {dataset}, {traceback.format_exc()}")
+        logging.error(
+            f"Failed to generate visualization: {dataset}, {traceback.format_exc()}"
+        )
         return f"Failed to generate visualization: {dataset}, {traceback.format_exc()}"
     return response.model_dump()
-    
+
 
 if __name__ == "__main__":
     if ENVIRONMENT == "remote":
